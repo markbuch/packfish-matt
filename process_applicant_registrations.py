38a8ca110833b480c500834318bc91094777d48d
Uncle Buck <gr_401@outlook.com>
2022-11-07 17:25:39 -0500

    added logging to script.

--- process_applicant_registrations.py
+++ process_applicant_registrations.py
@@ -4,12 +4,17 @@ from copy import deepcopy
 import config as cfg
 import csv
 import datetime
+import logging
 
 # define global variables
 results = []  # holds a list of applicants retrieved from the email form.
 applicant = {}  # Used to hold applicant data after extracting it from the email form
 fields = list(cfg.Header.fields.values()) # List of column headers for csv file
 CERTIFYING_VES = 'CERTIFYING_VES'
+
+# Setup logging and logfile
+log_filename = datetime.datetime.now().strftime("%m%d%Y_%H%M%S") + "_script_trace.log"
+logging.basicConfig(filename=log_filename, level=logging.DEBUG)
 
 
 # Functions
@@ -50,12 +55,14 @@ def export_results_to_csv():
     print(f'fields: {fields}')
     print(f'Results: {results}')
     csv_filename = datetime.datetime.now().strftime("%m%d%Y_%H%M%S") + "_session_import.csv"
+    logging.info(f'Exporting application results to file: {csv_filename}.')
     # open file and write
     with open(csv_filename, 'w', newline='') as csvfile:
         writer = csv.DictWriter(csvfile, fieldnames=fields)
         writer.writeheader()
         for item in results:
             writer.writerow(item)
+    logging.info('Finished exporting results to csv file.')
 
 
 def add_certifying_ves_to_applicant_data():
@@ -72,17 +79,19 @@ def add_certifying_ves_to_applicant_data():
 
 # main code
 def main():
-    print("Starting Script")
+    logging.info(f'Logging into Mail Server: {cfg.Mail.server}.')
     mb = MailBox(cfg.Mail.server).login(cfg.Mail.user, cfg.Mail.password)
-    print("Fetching application forms")
+
+    logging.info('Fetching application registration forms from mail server.')
     messages = mb.fetch(criteria=AND(from_="burst@emailmeform.com", seen=False), mark_seen=False, bulk=True)
 
     # Start processing retrieved applications
+    logging.info('Message process started...')
     for msg in messages:
-        print("Processing Messages")
+        logging.info('Processing next applicant')
         # parse html email message
         bs = BeautifulSoup(msg.html, 'html.parser')
-        # emailme form is output to a table.
+        # emailme form data resides in a html table.
         # get all table rows
         table_rows = bs.find('table').findAll('tr')
         # process each row to get the table data.
@@ -95,19 +104,21 @@ def main():
             # field name will contain '*:' and the will need to be stripped out.
             # the first td item should be the field name with *:, ex first name*:
             # the second td item should be the value
-
+            logging.info('\n')
             for item in td:
                 if item.text.find("*:") != -1:
                     name = item.text.strip().replace("*:", "")
                 else:
                     value = item.text.strip()
-            # before adding the name/value pair to the results dictionary, check for required modifications.
-            # Check for items that need updated before adding to csv file
+            # before adding the name/value pair to the applicant dict, check for required modifications.
+            logging.info(f'Performing pre-checks on: {name}, {value} ')
             if name == 'Middle Initial' and value.upper() == 'NONE':
                 # If Middle Initial is NONE, set value to empty string
+                logging.info('Middle Initial was set to NONE, replacing with empty string')
                 value = ''
             elif name == 'Suffix' and value.upper() == 'NONE':
                 # If Suffix is NONE, set value to empty string
+                logging.info('Suffix was set to NONE, replacing with empty string')
                 value = ''
             elif name == 'Street Address' and value.find('PO') == -1:
                 # If not a PO Box, add empty PO Box entry
@@ -118,36 +129,46 @@ def main():
                 value = ''
             elif name == 'Callsign' and value.upper() == 'NOCALL':
                 # If callsign is NOCALL, set Callsign value to empty string and set UPGRADE_LICENSE to False
+                logging.info('Callsign was sent to NONE, replacing with empty string.')
                 value = ''
                 applicant[cfg.Header.fields['UPGRADE_LICENSE']] = False
             elif name == 'Callsign' and value.isalnum():
                 # If a callsign was entered, set UPGRADE_LICENSE to True and convert callsign to upper case
+                logging.info(f'Callsign: {value.strip()} was detected, setting UPGRADE_LICENSE to true.')
                 applicant[cfg.Header.fields['UPGRADE_LICENSE']] = True
                 value = value.upper()
             elif name == 'Exams':
                 set_exams(value.split(', '))
 
-            print('=========================')
-            print(f'Name: {name}, Value: {value}')
+            logging.info(f'Name: {name}, Value: {value}')
+            logging.info('-' * 40)
 
             applicant[cfg.Header.fields[name]] = value.strip()
         
         # Add certifying VEs to applicant's data.
         add_certifying_ves_to_applicant_data()
-
-        print('Print applicant data')
-        print(applicant)
-        print('Add application to results list')
+        logging.info('Printing Applicant data...\n')
+        logging.info(f'Applicant: {applicant}\n')
+        # print('Print applicant data')
+        # print(applicant)
+        # print('Add application to results list')
+        logging.info('Adding applicant to results file.')
         results.append(deepcopy(applicant))
         applicant.clear()
 
     # Logout of mailbox
+    logging.info('Logging out of Mailbox.')
     mb.logout()
-    print('Results List')
-    print(results)
-    print('Export applicants to csv')
+    # print('Results List')
+    # print(results)
+    # print('Export applicants to csv')
     export_results_to_csv()
 
 
 if __name__ == '__main__':
+    logging.info(f'{datetime.datetime.now()} ===================================')
+    logging.info('Application export process starting')
+
     main()
+    logging.info('Application export process completed.')
+    logging.info(f'{datetime.datetime.now()} ===================================')
